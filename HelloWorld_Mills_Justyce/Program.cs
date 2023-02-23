c4e8c7d8101907c33deeb2058939363d3281037f
TheGamer798 <111769499+TheGamer798@users.noreply.github.com>
2023-02-23 09:40:16 -0600

    can say names

--- HelloWorld_Mills_Justyce/Program.cs
+++ HelloWorld_Mills_Justyce/Program.cs
@@ -6,7 +6,11 @@ namespace HelloWorld_Mills_Justyce
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your first name?");//asking for first name
+            string fisrtName = Console.ReadLine();//remembers first name
+            Console.WriteLine("What is your last name?");//asks for last name
+            string lastName = Console.ReadLine();//remembers last name
+            Console.WriteLine($"So your name is {fisrtName} {lastName}, interesting.");//tells you your full name
         }
     }
 }
